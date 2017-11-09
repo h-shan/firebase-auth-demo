b62fc2a865f1272da4c73ac89f33dd60581147f8
h-shan <howardshan11@gmail.com>
2017-11-09 14:31:39 -0600

    Initialize android project with firebase auth

---
+++ firebase-auth-demo-android/app/src/main/java/com/shan/howard/firebaseauthenticationdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.shan.howard.firebaseauthenticationdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+}
